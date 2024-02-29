a044162867f0d9064bc2382a5ee66adfc1eddaac
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-02-28 20:11:50 -0800

    Write function that returns possible moves given a coordinate on board

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './styles.css';
+
+function getPossibleMoves(coordinateArr) {
+  const coordChanges = [
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1],
+    [2, 1],
+    [1, 2],
+  ];
+
+  // apply changes to coordinates and keep
+  // moves that are on the board
+  const possibleMoves = coordChanges
+    .map((change) => [
+      coordinateArr[0] + change[0],
+      coordinateArr[1] + change[1],
+    ])
+    .filter((coord) => {
+      const x = coord[0];
+      const y = coord[1];
+      if (x >= 0 && x <= 7 && y >= 0 && y <= 7) return coord;
+      return false;
+    });
+  return possibleMoves;
+}
+
+console.log(getPossibleMoves([0, 3]));
